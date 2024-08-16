b1e947059093de91e60b74835560a8f9515af3a8
robertwilliampytel <robertwilliampytel@gmail.com>
2024-08-16 10:14:28 -0500

    added report printing function to main.py

--- main.py
+++ main.py
@@ -37,15 +37,36 @@ def number_of_chars(text_from_book: str) -> dict[str, int]:
     return chars
 
 
+def print_report(book_path: str) -> None:
+
+    print(f"--- Begin report of {book_path} ---")
+
+    file_text: str = get_book_text(book_path)
+    num_words: int = number_of_words(file_text)
+
+    print(f"{num_words} found in the document")
+    print()
+
+    num_chars: dict[str, int] = number_of_chars(file_text)
+
+    sorted_chars: list[tuple[str, int]] = list(num_chars.items())
+
+    def sort_on(tupl: tuple[str, int]) -> int:
+        return tupl[1]
+    
+    sorted_chars.sort(key=sort_on, reverse=True)
+
+    for tupl in sorted_chars:
+        print(f"The \'{tupl[0]}\' character was found {tupl[1]} times")
+    
+    print("--- End report ---")
+
+
 def main():
 
     book_path: str = "books/frankenstein.txt"
-    file_text: str = get_book_text(book_path)
-    num_words: int = number_of_words(file_text)
-    num_chars: dict[str, int] = number_of_chars(file_text)
 
-    print(num_words)
-    print(num_chars)
+    print_report(book_path=book_path)
 
 
 if __name__ == '__main__':
